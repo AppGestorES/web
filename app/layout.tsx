a3690e552967f1a40a02c8bc79b4d6474b858e41
Omar <118843137+Rakions@users.noreply.github.com>
2024-06-07 22:20:09 +0200

    Agregar analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./reset.css";
 import { inter } from "./fonts";
 import Header from "@/components/ui/index/header";
 import Footer from "@/components/ui/index/footer";
-import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import Script from "next/script";
 
 export const metadata: Metadata = {
@@ -17,20 +16,38 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
+        {/* Google Tag Manager */}
         <Script
           id="tagManager"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TDTH9WC9');
-    `,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TDTH9WC9');
+            `,
+          }}
+        />
+        {/* Google Analytics */}
+        <Script
+          id="analytics"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FJ3DZJC44H"
+        ></Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FJ3DZJC44H');
+            `,
           }}
         />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TDTH9WC9"
